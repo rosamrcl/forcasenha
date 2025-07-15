9db42af6a027197dfa26bd25fd47195439394eea
Rosa <164552124+rosamrcl@users.noreply.github.com>
2025-07-15 15:44:12 -0300

    .

---
+++ passwordStrong.php
@@ -0,0 +1,27 @@
+<?   
+class StrongPassword{
+    
+    private $value;
+    
+    public function __construct($value)
+    {
+        $this->value = $value;
+    }
+    public function isValid():bool{
+        return (
+            mb_strlen($this->value) >= 8 
+            && preg_match('/[A-Z]/', $this->value)
+            && preg_match('/[a-z]/', $this->value)
+            && preg_match('/[0-9]/', $this->value)
+            && preg_match('/[!@#$%^&*]/', $this->value)
+        
+        );
+        
+
+    }
+
+
+}
+
+
+?>
